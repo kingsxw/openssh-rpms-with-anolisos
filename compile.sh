ae0fe79bf7c689faf26979d54b60d26f5dcaf314
kingsxw <kingsxw@163.com>
2025-03-06 08:29:08 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -91,7 +91,9 @@ BUILD_RPM() {
 	    install -v -m666 $__dir/downloads/$fn ./SOURCES/
 	done
 
-	rpmbuild -bb SPECS/openssh.spec --target $(uname -m) --nodebuginfo \
+	rpmbuild -bb SPECS/openssh.spec \
+		--target $(uname -m) \ 
+ 	#	--nodebuginfo \
  		--define "_topdir $PWD" \
 		--define "debug_package %{nil}" \
 		--define "opensslver ${OPENSSLVER}" \
